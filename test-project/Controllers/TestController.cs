98368d8829b43cb18bd48e768f6e04e2d6753130
Saharia Hossain <biplob65712@gmail.com>
2022-09-27 14:27:47 +0600

    comment-message

---
+++ test-project/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_project.Controller
+{
+    public class TestController : ControllerBase
+    {
+        // GET: TestController
+        public ActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
